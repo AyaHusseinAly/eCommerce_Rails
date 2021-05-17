de581f1beb03535a40732af1cefcb85a9f81cbff
Fatma-Tarek <fatma.tarek204@gmail.com>
2021-05-17 22:07:37 +0200

    search for products by title or descriptions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,17 @@ class HomeController < ApplicationController
         @products=Product.all.order("created_at desc").limit(12)
         @categories=Category.all
     end
+
+    def searchInProduct
+        #if params[:q] == ""
+            @products = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                Product.where("description LIKE ?","%"+ params[:q] +"%")
+            )
+            @categories=Category.all
+        #else
+           # return RedirectToAction("index", "home");
+          #  return RedirectToAction("index", "home")
+        #end
         
-end
+    end
+end 
