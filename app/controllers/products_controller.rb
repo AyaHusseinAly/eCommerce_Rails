807016960f1a9ba0fd9017ca4808cb16ac0060a4
aya hussein <aya.hussein3107@gmail.com>
2021-05-17 06:22:58 +0200

    user can review products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,24 @@ class ProductsController < ApplicationController
 
       def show
         @product = Product.find(params[:id])
-        #puts "*"*50
-        #puts @product.rate_reviews
+        number = @product.rate_reviews.count
+        rating=0
+        @product.rate_reviews.each { |n| rating+=n.rating.to_i }
+        @avg=(rating/number)
+        @avg_rate=@avg.round
+      end
+
+      def rate
+        @product = Product.find(params[:id])
+        @rate_review = @product.rate_reviews.new
+        @rate_review.content="whatever for test"
+        @rate_review.rating=params[:rating]
+        if user_signed_in?
+            @rate_review.user=current_user
+        end
+        @rate_review.save()
+
+        redirect_to product_path(@product)
+        
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'api/products', to: "api#index"
   get 'api/products/:id', to: "api#show"
+  get '/products/:id/:rating' => 'products#rate', as: :rate_product
+
   resources :categories
-  resources :products
+  resources :products do
+    resources :rate_reviews
+  end
   root 'home#index'
 end
