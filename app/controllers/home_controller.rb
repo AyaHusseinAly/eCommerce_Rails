e618582f7b0ae7eafcdc8b3ec625984f00f0a898
Fatma-Tarek <fatma.tarek204@gmail.com>
2021-05-18 23:50:23 +0200

    add filter by name and filter with catogery, brand, price and search by name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,116 @@ class HomeController < ApplicationController
         @categories=Category.all
     end
 
+
     def searchInProduct
         if params[:q] != ""
             if params[:cat] == "All" and params[:cat] == "All" and params[:price] == "All" and params[:seller] == "All" and params[:q] !=""
                 @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
                     Product.where("description LIKE ?","%"+ params[:q] +"%")
                 )
+            elsif params[:cat] != "All"
+                @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                    Product.where("description LIKE ?","%"+ params[:q] +"%")
+                ).where(category: params[:cat])
             end
 
+            if params[:cat] == "All"
+                if params[:brand] == "All" 
+                    if params[:price] == "All"
+                        if  params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(store_id: params[:seller])
+                        end
+                    elsif params[:price] != "All"
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(price: params[:price])
+                        elsif params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(store_id: params[:seller], price: params[:price])
+                        end
+                    end
+
+                ##!! second case !!##
+                elsif  params[:brand] != "All"
+                    if  params[:price] == "All"
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(brand_id: params[:brand])
+                        elsif params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(store_id: params[:seller] ,brand_id: params[:brand] )
+                        end
+                    elsif params[:price] != "All"   
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(brand_id: params[:brand], price: params[:price])
+                        elsif params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(brand_id: params[:brand], price: params[:price],store_id: params[:seller])
+                        end
+                    end
+                end #end of brand if exist or not 
+             ##!! Third case !!##
+             
+            elsif params[:cat] != "All"
+                if params[:brand] == "All" 
+                    if  params[:price] == "All"
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat])
+                        elsif params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(store_id: params[:seller] ,category: params[:cat] )
+                        end
+                    elsif params[:price] != "All" 
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat], price: params[:price])
+                        elsif  params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat], price: params[:price],store_id: params[:seller])
+                        end
+                    end
+                
+                elsif  params[:brand] != "All"
+                    if params[:price] == "All"
+                        if params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat], brand_id: params[:brand])
+                        elsif params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(store_id: params[:seller] ,category: params[:cat],brand_id: params[:brand] )
+                        end
+                    elsif params[:price] != "All"  
+                        if  params[:seller] == "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat], price: params[:price],brand_id: params[:brand])
+                        elsif  params[:seller] != "All"
+                            @products1 = Product.where("title LIKE ?","%"+ params[:q] +"%").or(
+                                Product.where("description LIKE ?","%"+ params[:q] +"%")
+                                ).where(category: params[:cat], price: params[:price],store_id: params[:seller],brand_id: params[:brand])
+                        end 
+                    end
+                end
+
+            end #end if cat is exist or not 
+
+        ################### select without name ################# 
         else
             ############!!!!!!!!!!!!!!!!!!!!################
             ## first probably ##
@@ -86,7 +188,6 @@ class HomeController < ApplicationController
 ##################################################################
         @products=Product.all
         @categories=Category.all 
-        @brands=Brand.all
-            
+        @brands=Brand.all     
     end
 end 
