129dda3758cfed95fc4cf38b837adf7b280be58b
Fatma-Tarek <fatma.tarek204@gmail.com>
2021-05-20 22:08:20 +0200

    solve issues

--- db/migrate/20210513171242_create_orders.rb
+++ db/migrate/20210513171242_create_orders.rb
@@ -1,11 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
-  def change
-    create_table :orders do |t|
-      t.integer :user_id
-      t.string :status
-      t.integer :coupon_id
+   def change
+     create_table :orders do |t|
+       t.integer :user_id
+       t.string :status
+       t.integer :coupon_id
 
-      t.timestamps
-    end
+       t.timestamps
+     end
   end
 end
