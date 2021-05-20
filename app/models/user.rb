511e2578967cb9607f9c69407b0f2b60556a9392
[suzan naeem] <[suzannaim7@gmail.com]>
2021-05-20 05:54:25 +0200

    add avatar of user and edit navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,32 @@ class User < ApplicationRecord
     has_many :wishing_list_items
     has_many :rate_reviews
     has_many :shopping_card_items
-    has_many :stores    
+    has_many :stores 
+    has_one_attached :avatar  
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+    def avatar_thumbnail
+      if avatar.attached?  
+         avatar.variant(resize: "250x200!").processed
+        # resource.avatar.attach(params[:avatar])
+        
+      else
+        "/default_profile.jpg"
+      end
+    end 
+    
+  private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app','assets','images','default_profile.jpg'
+          )
+        ), filename: 'default_profile.jpg',
+        content_type: 'image/jpg'
+      ) 
+    end
+  end       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_004043) do
+ActiveRecord::Schema.define(version: 2021_05_19_192026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "brands", force: :cascade do |t|
@@ -75,13 +70,13 @@ ActiveRecord::Schema.define(version: 2021_05_18_004043) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
     t.string "status"
+    t.integer "coupon_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "coupon_id"
-    t.bigint "user_id"
-    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.bigint "{:foreign_key=>true}_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_orders_on_{:foreign_key=>true}_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -128,8 +123,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_004043) do
     t.text "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -156,10 +149,9 @@ ActiveRecord::Schema.define(version: 2021_05_18_004043) do
     t.index ["user_id"], name: "index_wishing_list_items_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "order_details", "orders"
   add_foreign_key "order_details", "products"
-  add_foreign_key "orders", "coupons"
-  add_foreign_key "orders", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "stores"
@@ -167,7 +159,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_004043) do
   add_foreign_key "rate_reviews", "users"
   add_foreign_key "shopping_card_items", "products"
   add_foreign_key "shopping_card_items", "users"
-  add_foreign_key "stores", "users"
   add_foreign_key "wishing_list_items", "products"
   add_foreign_key "wishing_list_items", "users"
 end
