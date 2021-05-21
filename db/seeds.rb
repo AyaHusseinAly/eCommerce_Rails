f6ead4b103d6173000ff09688272c3b5b635b6a7
shrook <shrooktageldin@gmail.com>
2021-05-21 20:30:31 +0200

    add to wishing list senario , add to cart senario ,make order

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,12 @@ User.destroy_all
 Brand.destroy_all
 Category.destroy_all
 AdminUser.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!(Product.table_name)
-ActiveRecord::Base.connection.reset_pk_sequence!(Store.table_name)
-ActiveRecord::Base.connection.reset_pk_sequence!(Brand.table_name)
-ActiveRecord::Base.connection.reset_pk_sequence!(Category.table_name)
-ActiveRecord::Base.connection.reset_pk_sequence!(User.table_name)
-ActiveRecord::Base.connection.reset_pk_sequence!(AdminUser.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(Product.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(Store.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(Brand.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(Category.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(User.table_name)
+# ActiveRecord::Base.connection.reset_pk_sequence!(AdminUser.table_name)
 
 
 
@@ -48,12 +48,12 @@ Category.create!([{
     name: "Women's Clothes"
 }
 ])
-User.create!(
-    name:"User",
-    email:"user@example.com",
-    password:"password",
-    password_confirmation: 'password'
-)
+# User.create!(
+#     name:"User",
+#     email:"user@example.com",
+#     password:"password",
+#     password_confirmation: 'password'
+# )
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 AdminUser.create!(email: 'seller@example.com', password: 'password', password_confirmation: 'password',role: 'seller') if Rails.env.development?
 
