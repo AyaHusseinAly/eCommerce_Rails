0d0c28621969c4a26e65ae24698b730aded38a3b
unknown <samar.mohamed.5523@gmail.com>
2021-05-21 21:02:44 -0700

    pull upadte

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_192026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,18 +34,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_192026) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "brands", force: :cascade do |t|
@@ -96,13 +70,13 @@ ActiveRecord::Schema.define(version: 2021_05_19_192026) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
     t.string "status"
+    t.integer "coupon_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "coupon_id"
-    t.bigint "user_id"
-    t.index ["coupon_id"], name: "index_orders_on_coupon_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.bigint "{:foreign_key=>true}_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_orders_on_{:foreign_key=>true}_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -157,7 +131,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_192026) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role", default: "buyer"
     t.text "address"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -180,8 +153,6 @@ ActiveRecord::Schema.define(version: 2021_05_19_192026) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "order_details", "orders"
   add_foreign_key "order_details", "products"
-  add_foreign_key "orders", "coupons"
-  add_foreign_key "orders", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "stores"
---
+++ test/controllers/coupon_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class CouponControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def new
+  end
+end
