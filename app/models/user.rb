48107590831c892836534c264486383789a63874
suzan-naeem <64129170+suzan-naeem@users.noreply.github.com>
2021-05-24 14:12:11 +0200

    update password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,48 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true , format: { with: /\A[a-zA-Z]+\z/,
   message: "only allows letters" }, uniqueness: true
-  validates :password, :password_confirmation, presence: true , length: { minimum:6 }
+#   validates :password, :password_confirmation, presence: true , length: { minimum:6 }
+  
+  PASSWORD_FORMAT = /\A
+
+  (?=.{6,})          # Must contain 6 or more characters
+
+  (?=.*\d)           # Must contain a digit
+
+  (?=.*[a-z])        # Must contain a lower case character
+
+  (?=.*[A-Z])        # Must contain an upper case character
+
+/x
+
+
+
+ validates :password, 
+
+  presence: true, 
+
+  length: { in: Devise.password_length }, 
+
+  format: { with: PASSWORD_FORMAT }, 
+
+  confirmation: true, 
+
+  on: :create 
+
+
+
+ validates :password, 
+
+  allow_nil: true, 
+
+  length: { in: Devise.password_length }, 
+
+  format: { with: PASSWORD_FORMAT }, 
+
+  confirmation: true, 
+
+  on: :update
+  
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
